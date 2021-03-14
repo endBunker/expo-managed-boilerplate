46d5546dd0efd5d2963bba0c31296c920d2014e3
Giuseppe Lanzillotta <g.lanzillotta@protonmail.com>
2021-03-14 13:17:25 +0100

    fix jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-  return {
-    verbose: true,
-    preset: "jest-expo",
-    transformIgnorePatterns: [
-      "node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
-    ],
-  }
-}
--- screen/Home.tsx
+++ screen/Home.tsx
@@ -7,12 +7,14 @@ import i18n from '../i18n'
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{i18n.t('common.welcome')}</Text>
+      <Text style={styles.text} testID='welcome'>
+        {i18n.t('common.welcome')}
+      </Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: tailwind('flex-1 bg-white items-center justify-center'),
-  text: tailwind('text-lg font-light')
+  text: tailwind('text-lg font-light'),
 })
