3e1b07973b96b334fb16d5e5784897b1fee9948f
endBunker <g.lanzillotta@protonmail.com>
2021-07-29 11:37:14 +0200

    upgrade packages and add dotenv support

---
+++ app.config.js
@@ -0,0 +1,32 @@
+import 'dotenv/config'
+
+export default {
+  name: 'expo-managed-boilerplate',
+  slug: 'expo-managed-boilerplate',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  updates: {
+    fallbackToCacheTimeout: 0,
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    package: 'com.bunker.boilerplate',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  extra: {},
+}
